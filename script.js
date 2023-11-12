1a3a660f6787ab9d1d94ab0d0b6ae4a3f5e31f16
Carina Grode <106753609+carinagrode@users.noreply.github.com>
2023-11-12 09:42:55 +0100

    Create script and variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const input = document.getElementById('userInput');
+const enterButton = document.getElementById('enter');
+const list = document.querySelector('ul');
+const item = document.getElementsByTagName('li');
